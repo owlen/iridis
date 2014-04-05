563836d93ece7b1728c9e3b88019489892ce6d08
Israel <israel@ludi>
2014-04-05 03:47:50 +0300

    Messaging works nice with no filter
    TODO: filter

--- messages.py
+++ messages.py
@@ -1,21 +1,50 @@
 #!/usr/bin/python3
 
+# Connect to bitmessage - user and pass configured with:
+# https://github.com/Dokument/PyBitmessage-Daemon
+import xmlrpc.client
+bitmessage = xmlrpc.client.ServerProxy('http://admin:123@127.0.0.1:6713')
+
+# UTF-8 to base64 and back
+from base64 import b64encode, b64decode
+def b64enc(s): return b64encode(bytes(s, 'utf-8')).decode('utf-8')
+def b64dec(b): return b64decode(b).decode('utf-8')
+
+# Propose and offer are just wrappers of send
+from json import dumps as jsdumps
+toaddress = '2cUEpXgRYZAmVqqWd8tzAFLr8UXtCbZy4g'
+fromaddress = '2cW9hFSVrVs2AcqwKgkcx6QtL6fgXNa4AP'
+def send(subject, message):
+    if not type(message) is str: message = jsdumps(message)
+    subject, message = map(b64enc, (subject, message))
+    return bitmessage.sendMessage(toaddress, fromaddress, subject, message)
+def propose(filter): return send('proposition', filter)
+def offer(tx): return send('offer', tx)
+
+# Get a list of all new messages
+from json import loads as jsloads
 def listen(filter):
-    return []
+    messages = []
+    inbox = jsloads(bitmessage.getAllInboxMessages())['inboxMessages']
+    for msgid in (m['msgid'] for m in inbox):
+        message = jsloads(bitmessage.getInboxMessageByID(msgid))['inboxMessage'][0]
+        subject, message = map(b64dec, (message['subject'], message['message']))
+        messages.append({'subject': subject, 'message': message})
+        bitmessage.trashMessage(msgid)
+    return messages
 
-def propose(filter):
-    return True
-
-def offer(tx):
-    return True
-
+# Serve RPC
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 server = SimpleXMLRPCServer(("127.0.0.1", 6712), requestHandler=SimpleXMLRPCRequestHandler)
 server.register_introspection_functions()
 
-server.register_function(listen, 'listen')
 server.register_function(propose, 'propose')
 server.register_function(offer, 'offer')
+server.register_function(listen, 'listen')
 
-server.serve_forever()
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    from sys import exit
+    exit(0)
--- trader.py
+++ trader.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 import xmlrpc.client
-
-colorserver = xmlrpc.client.ServerProxy('http://127.0.0.1:6711')
-print(colorserver.getvoutcolor('1'))
-print(colorserver.system.listMethods())
-
 msgserver = xmlrpc.client.ServerProxy('http://127.0.0.1:6712')
-print(msgserver.listen('1'))
-print(msgserver.system.listMethods())
+print("Connected to message server with functions: %s" % (', '.join(msgserver.system.listMethods())))
+
+#colorserver = xmlrpc.client.ServerProxy('http://127.0.0.1:6711')
+#print(colorserver.getvoutcolor('1'))
+#print(colorserver.system.listMethods())
+
+try:
+    while True:
+        cmd = input("\nType command ([P]ropose, [O]ffer, [I]nbox, [Q]uit)\n")
+        if 'q' == cmd.lower(): break
+        if 'i' == cmd.lower():
+            print(msgserver.listen(''))
+            continue
+        if 'o' == cmd.lower():
+            print(msgserver.offer(input("\noffer\n")))
+            continue
+        if 'p' == cmd.lower():
+            print(msgserver.propose(input("\npropose\n")))
+            continue
+except KeyboardInterrupt:
+    from sys import exit
+    exit(0)
