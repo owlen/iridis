9ad7a8f45475db1108e964ca14db27ac36e12127
Israel <israel@ludi>
2014-04-13 12:46:53 +0300

    Updated README

--- color.py
+++ color.py
@@ -1,41 +1,15 @@
 #!/usr/bin/python3
 
-def getvoutcolor(vout):
-    return {
-    "name": "Credit at John Doe's",
-    "name_short": "JD Credits",
-    "issuer": "John Doe's Restaurant Chain",
-    "type": "Credit",
-    "currency": "USD",
-    "description": "Can be used for any purchase at John Doe's, excluding breakfasts.",
+def getoutputvalue(output):
+    return { 'colorvalue': 1 }
 
-    "interest_rate": 1,
-    "issue_date": "2014-03-01",
-    "expiry_date": "2024-02-29",
-
-    "icon_url": "http://www.john-doe-dining.com/bitcoin-credits-icon.png",
-    "image_url": "http://www.john-doe-dining.com/bitcoin-credits-image.png",
-    "contract_url": "http://www.john-doe-dining.com/bitcoin-credits-contract.pdf",
-    "redemption_url": "http://www.john-doe-dining.com/bitcoin-credits-redeem.php",
-    "feed_url": "http://www.john-doe-dining.com/bitcoin-credits-feed.rss",
-
-    "color": "#ffccaa",
-    "multiple": 0.01,
-    "format": "* dollars",
-    "format_1": "1 dollar",
-
-    "mastercoin_id": "123456",
-    "coinprism_sources": ["2MyvmsxoCxsnzPapDnK7ZcMxWjU3hqLZkpX"],
-    "coincolors_id": "f5d8ee39a430901c91a5917b9f2dc19d6d1a0e9cea205b009ca73dd04470b9a6"
-}
-
-def makeconversion(sieve):
+def makeconversion(color, amount1, key, outputs, amount2):
     return { 'hex': 'tx' }
 
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
 server = SimpleJSONRPCServer(('127.0.0.1', 6711))
 server.register_introspection_functions()
-server.register_function(getvoutcolor, 'getvoutcolor')
+server.register_function(getoutputvalue, 'getoutputvalue')
 server.register_function(makeconversion, 'makeconversion')
 print('Serving JSON on 6711')
 try:
--- trader.py
+++ trader.py
@@ -3,12 +3,42 @@
 import jsonrpclib
 msgserver = jsonrpclib.Server('http://127.0.0.1:6712')
 print("Connected to message server with functions: %s" % (', '.join(msgserver.system.listMethods())))
-#colorserver = jsonrpclib.Server('http://127.0.0.1:6711')
-#print("Connected to color server with functions: %s" % (', '.join(colorserver.system.listMethods())))
+colorserver = jsonrpclib.Server('http://127.0.0.1:6711')
+print("Connected to color server with functions: %s" % (', '.join(colorserver.system.listMethods())))
 
 # Here ars issuances of red and blue from chromawallet:
 unspentred = ['8a78b017ea9d97ca7d1044fc9b3bc40356518a9fe68a4466197c0f22d2e29e82', 1]
 unspentblue = ['6beaf35c6b4fbb8dfa0c4a1483e90ecd06d2ecbd4a05b2dbe92a480acd4ad2fe', 1]
+
+# And here is a JSON that defines it, according to the mastercoin definitions: https://github.com/mastercoin-MSC/spec/blob/master/AssetIssuanceStandard.md
+# TODO: Create to JSONs for red and blue, and start using them.
+colordef = {
+    "name": "Credit at John Doe's",
+    "name_short": "JD Credits",
+    "issuer": "John Doe's Restaurant Chain",
+    "type": "Credit",
+    "currency": "USD",
+    "description": "Can be used for any purchase at John Doe's, excluding breakfasts.",
+
+    "interest_rate": 1,
+    "issue_date": "2014-03-01",
+    "expiry_date": "2024-02-29",
+
+    "icon_url": "http://www.john-doe-dining.com/bitcoin-credits-icon.png",
+    "image_url": "http://www.john-doe-dining.com/bitcoin-credits-image.png",
+    "contract_url": "http://www.john-doe-dining.com/bitcoin-credits-contract.pdf",
+    "redemption_url": "http://www.john-doe-dining.com/bitcoin-credits-redeem.php",
+    "feed_url": "http://www.john-doe-dining.com/bitcoin-credits-feed.rss",
+
+    "color": "#ffccaa",
+    "multiple": 0.01,
+    "format": "* dollars",
+    "format_1": "1 dollar",
+
+    "mastercoin_id": "123456",
+    "coinprism_sources": ["2MyvmsxoCxsnzPapDnK7ZcMxWjU3hqLZkpX"],
+    "coincolors_id": "f5d8ee39a430901c91a5917b9f2dc19d6d1a0e9cea205b009ca73dd04470b9a6"
+}
 
 # Alice owns the red coins, and wishes to sell 10 of them for 10 blue coins.
 # Note that offeredOutputs is a list of unspent outputs, while requestedAsset is a single blue output, and can be any such output, even a spent one. It is just a way to represent a color.
